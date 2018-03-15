e955cc71e4966ec111ca9973fbb9b3c55cf8ef86
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-03-15 19:52:55 +0900

    add

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import "./App.css";
 import Child from "./Child.jsx";
 import { ToastContainer, toast } from "react-toastify";
 
+const ModalMessage = () => {
+  return (
+    <div>
+      <a href="/">New Content Available! please reload</a>
+    </div>
+  );
+};
+
 class App extends Component {
   componentDidMount() {
     window.addEventListener("newContentAvailable", () => {
-      toast("New Content Available! please reload");
+      toast(() => <ModalMessage />, { autoClose: false });
     });
   }
   render() {
     return (
       <div className="App">
-        <ToastContainer />
+        <ToastContainer autoClose={false} />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome tzzs React</h1>
--- src/Child.jsx
+++ src/Child.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
 export default () => {
-  return <div>Childrenww</div>;
+  return <div>Children</div>;
 };
