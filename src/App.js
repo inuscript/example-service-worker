17c9edab856b0638b292cce51c9e83dc3c6dc856
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-03-15 19:42:34 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Child from "./Child.jsx"
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Child from "./Child.jsx";
+import { ToastContainer, toast } from "react-toastify";
+
 class App extends Component {
+  componentDidMount() {
+    window.addEventListener("newContentAvailable", () => {
+      toast("New Content Available! please reload");
+    });
+  }
   render() {
     return (
       <div className="App">
+        <ToastContainer />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome tzzs React</h1>
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -10,16 +10,16 @@
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === '[::1]' ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
 );
 
 export default function register() {
-  // if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  // if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) { // for testing
   if ('serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
@@ -33,7 +33,7 @@ export default function register() {
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
       console.log("yay load")
-        
+
       if (isLocalhost) {
         // This is running on localhost. Lets check if a service worker still exists or not.
         checkValidServiceWorker(swUrl);
@@ -42,7 +42,7 @@ export default function register() {
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://goo.gl/SC7cgQ'
+            'worker. To learn more, visit https://goo.gl/SC7cgQ'
           );
         });
         registerValidSW(swUrl);
@@ -70,8 +70,8 @@ function registerValidSW(swUrl) {
               // It's the perfect time to display a "New content is
               // available; please refresh." message in your web app.
               console.log('New content is available; please refresh.');
-              window.location.reload();
-
+              const event = new Event('newContentAvailable');
+              window.dispatchEvent(event);
             } else {
               // At this point, everything has been precached.
               // It's the perfect time to display a
